f1a42d133e9f4b709ea6562a5f54cf7acc7139b2
Sergio <sergiohdezmoya@usal.es>
2024-10-17 23:04:04 +0200

    commit

--- semana05/ejercicio1/src/modelo/autor.java
+++ semana05/ejercicio1/src/modelo/autor.java
@@ -1,9 +1,21 @@
 package modelo;
 
-public class autor {
+public class Autor {
     private String nombre;
     private String apellidos;
     private boolean premioPlaneta;
 
-    
+    public Autor(String nombre, String apellidos, boolean premioPlaneta) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.premioPlaneta = premioPlaneta;
+    }
+
+    public String getNombreCompleto() {
+        return nombre + " " + apellidos;
+    }
+
+    public boolean isPremioPlaneta() {
+        return premioPlaneta;
+    }
 }
--- semana05/ejercicio1/src/modelo/libro.java
+++ semana05/ejercicio1/src/modelo/libro.java
@@ -1,5 +1,41 @@
 package modelo;
 
-public class libro {
-private String nombre;
+public class Libro {
+    private Autor autor;
+    private String titulo;
+    private int Publicacion;
+    private int numPaginas;
+    private float precio;
+
+    public Libro(Autor autor, String titulo, int Publicacion, int numPaginas, double precio) {
+        this.autor = autor;
+        this.titulo = titulo;
+        this.Publicacion = Publicacion;
+        this.numPaginas = numPaginas;
+        this.precio = precio;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public int getPublicacion() {
+        return Publicacion;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public int getNumPaginas() {
+        return numPaginas;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public float calcularTiempoLectura(int ritmoLectura) {
+        return (float) numPaginas / ritmoLectura;
+    }
 }
